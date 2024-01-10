26046ac48fd7d50e12251ef6a40e95528aaf02e5
m-wambua <michaelmutisyawambua@gmail.com>
2024-01-10 23:38:05 +0300

    added the My Services page

--- flutter_application_1/lib/globals/app_assets.dart
+++ flutter_application_1/lib/globals/app_assets.dart
@@ -1,4 +1,5 @@
 class AppAsset {
   static String linkedin = 'assets/image/in.png';
   static String github = 'assets/image/git.png';
+  static String code = 'assets/image/coding.png';
 }
---
+++ flutter_application_1/lib/my_services.dart
@@ -0,0 +1,108 @@
+import 'package:animate_do/animate_do.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/globals/app_assets.dart';
+import 'package:flutter_application_1/globals/app_buttons.dart';
+import 'package:flutter_application_1/globals/app_colors.dart';
+import 'package:flutter_application_1/globals/app_text_styles.dart';
+
+class MyServices extends StatelessWidget {
+  const MyServices({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
+    return Container(
+      width: size.width,
+      height: size.height,
+      color: AppColors.bgColor,
+      child: Column(children: [
+        FadeInRight(
+          duration: const Duration(milliseconds: 1200),
+          child: RichText(
+            text: TextSpan(
+                text: 'My',
+                style: AppTextStyles.AboutMeHeading(),
+                children: [
+                  TextSpan(text: ' Services', style: AppTextStyles.AboutMeMe())
+                ]),
+          ),
+        ),
+        const SizedBox(
+          height: 40,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            buildAnimatedContainer(
+                title: 'App Development', asset: AppAsset.code),
+            const SizedBox(
+              width: 15,
+            ),
+            buildAnimatedContainer(
+                title: 'Machine Learning', asset: AppAsset.code),
+            const SizedBox(
+              width: 15,
+            ),
+            buildAnimatedContainer(title: 'Data Analysis', asset: AppAsset.code)
+          ],
+        )
+      ]),
+    );
+  }
+
+  AnimatedContainer buildAnimatedContainer(
+      {required String title, required String asset}) {
+    return AnimatedContainer(
+      duration: const Duration(
+        milliseconds: 600,
+      ),
+      width: 450,
+      //height: 400,
+      alignment: Alignment.center,
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 25),
+      decoration: BoxDecoration(
+          color: AppColors.bgColor2,
+          borderRadius: BorderRadius.circular(30),
+          boxShadow: const [
+            BoxShadow(
+                color: Colors.black54,
+                spreadRadius: 4.0,
+                blurRadius: 4.5,
+                offset: Offset(3.0, 4.5))
+          ]),
+
+      child: Column(children: [
+        Image.asset(
+          AppAsset.code,
+          width: 50,
+          height: 50,
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        Text(
+          'App Development',
+          style: AppTextStyles.nameTextStyle(),
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        Text(
+          'Welcome! I\'m Michael Wambua,\n'
+          'an Electrical Engineering grad passionate \n'
+          'about tech-driven innovation in energy. \n'
+          'As a Flutter Developer and Machine Learning Engineer ,\n'
+          'I blend technical expertise with hands-on experience.\n'
+          'Explore my portfolio for cutting-edge solutions and\n'
+          'sustainable practices. Let\'s innovate together!',
+          style: AppTextStyles.descriptionStyle(),
+          textAlign: TextAlign.center,
+        ),
+        const SizedBox(
+          height: 15,
+        ),
+        AppButtons.buildMaterialButton(buttonName: 'Read More', onTap: () {})
+      ]),
+    );
+  }
+}
